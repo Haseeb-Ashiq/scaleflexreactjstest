5fe090df6e2586542aab64a3148a25e03795b86c
mhaseeb <haseeahmad7862@gmail.com>
2022-06-13 16:26:51 +0500

    anothercommit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import BarChart from './components/barchart';
 import LineChart from './components/linechart';
 
 function App() {
+  const data=[50,25,34,12,120,200,56,23,34,11];
+  const data2=[500,300,444,555,666,777,444,4433,222,3333,555,555];
   return (
     <div className="App">
-    <BarChart/>
-    <LineChart/>
+    <BarChart
+    mode={'bar'}
+    data={data}
+    />
+    <LineChart
+    data={data2}
+    />
     </div>
   );
 }
--- src/components/barchart.js
+++ src/components/barchart.js
@@ -1,20 +1,24 @@
 import React, { useEffect } from "react";
-const BarChart=()=>{
+const BarChart=(props)=>{
     useEffect(()=>{
         const drawBarChart=(data)=>{
             let html='';
+            // let month='';
             let x=0;
             for(let i=0;i<data.length;i++)
             {
                 html+=`<rect x=${x} y=${500-data[i]} width='20' height=${data[i]} />`;
                 x+=25;
             }
+            // console.log(month)
             document.getElementById('bar_chart').innerHTML=html;
+            // document.querySelector('.month_name').innerHTML=month;
         }
-        drawBarChart([50,25,34,12,120,200,56,23,34,11]);
+        drawBarChart(props.data);
     },[])
     return (<>
     <svg width='400' height='500' id='bar_chart' fill='lightblue' style={{border:'1px solid black'}}></svg>
+    <div className="month_name" style={{width:'400px',border:'1px solid black',display:'flex',margin:'0 auto'}}></div>
     </>)
 }
 export default BarChart;
